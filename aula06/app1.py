b3d67c863c68a1de94a5a27888cf6deed9c87be7
mariaed97 <msousarodrigues135@gmail.com>
2025-08-26 12:07:06 -0300

    aula06

---
+++ aula06/app1.py
@@ -0,0 +1,18 @@
+# dicionario 
+
+usuario = {
+    'nome': 'maria',
+    'idade': 17
+}
+
+print(type(usuario))
+print(usuario)
+
+# print(f'Nome: {usuario["nome"]}')
+# print(f'Idade: {usuario["idade"]}')
+
+usuarios = ['gomes', 'maria', 'cris', 'nunes']
+print(usuarios)
+print(f'Nome: {usuarios[-1]}')
+
+print(f'Nome: {usuario.get('nome')}')
